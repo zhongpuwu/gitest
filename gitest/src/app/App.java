776a85b908a334929033c5a1255bb5047484635b
zhongpuwu <418743540@qq.com>
2019-04-16 22:06:28 +0800

    2

--- gitest/src/app/App.java
+++ gitest/src/app/App.java
@@ -1,7 +1,7 @@
 package app;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+    public static void main(String[] args) {
+        System.out.println("Hello github");
     }
 }
